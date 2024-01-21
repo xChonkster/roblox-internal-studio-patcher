16e9c923d60b57be1bd2e75ec509c6e5c53f71a9
xChonkster <xchonkster@gmail.com>
2024-01-21 21:55:45 +0100

    fix sigs

--- roblox-internal-studio-patcher/src/roblox-internal-studio-patcher.cpp
+++ roblox-internal-studio-patcher/src/roblox-internal-studio-patcher.cpp
@@ -31,28 +31,31 @@ int main(int argc, char* argv[])
 		// we'll be writing into this
 		studio.read( reinterpret_cast<char*>( file_buffer ), file_size );
 
-		const auto scan_write_zero_to_flag = memory::aob_scan( file_buffer, file_size, "xxxxxxxxxxxxxxxx", { 0xC6, 0x05, 0x1F, 0x26, 0xFC, 0x04, 0x00, 0x48, 0x8D, 0x0D, 0x1E, 0x26, 0xFC, 0x04, 0xE8, 0xB9 } );
+		// C6 05 ? ? ? ? 00 48 8D 0D ? ? ? ? E8
+		const auto scan_write_zero_to_flag = memory::aob_scan( file_buffer, file_size, "xx????xxxx????x", { 0xC6, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x8D, 0x0D, 0x00, 0x00, 0x00, 0x00, 0xE8 } ); // (i kinda need a "?" symbol)
 
 		if ( scan_write_zero_to_flag.empty() )
 			throw std::runtime_error{ "Sig not matched (image is already patched, or aob is outdated)" };
 
 		// change write 0 to write 1
-		*reinterpret_cast<std::uint8_t*>( file_buffer + scan_write_zero_to_flag[0] + 6 ) = 0x01;
+		*reinterpret_cast<std::uint8_t*>( file_buffer + scan_write_zero_to_flag[8] + 6 ) = 0x01;
 
-		const auto scan_write_a1_to_flag = memory::aob_scan( file_buffer, file_size, "xxxxxxxxxxxxxxxx", { 0x88, 0x0D, 0x12, 0x4A, 0x9D, 0x04, 0xC3, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC } );
+		// CC 88 0D ? ? ? ? C3 CC
+		const auto scan_write_a1_to_flag = memory::aob_scan( file_buffer, file_size, "xxx????xx", { 0xCC, 0x88, 0x0D, 0x00, 0x00, 0x00, 0x00, 0xC3, 0xCC } );
 
 		// always write 1 to variable instead of a1
 		const std::uint8_t patch[] =
 		{
-			0xC6, 0x05, 0x12, 0x4A, 0x9D, 0x04, 0x01, 0xC3, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC
+			0xCC, 0xC6, 0x05, 0x12, 0x4A, 0x9D, 0x04, 0x01, 0xC3, 0xCC
 		};
 
-		std::memcpy( file_buffer + scan_write_a1_to_flag[0], patch, sizeof( patch ) );
+		std::memcpy( file_buffer + scan_write_a1_to_flag[7], patch, sizeof( patch ) );
 
-		const auto scan_soothsayer_check = memory::aob_scan( file_buffer, file_size, "xxxxxxxxxx", { 0x80, 0xBF, 0x78, 0x01, 0x00, 0x00, 0x00, 0x74, 0x05, 0xE8 } );
+		// E8 ? ? ? ? 80 BF ? ? ? ? ? 74 05 E8
+		const auto scan_soothsayer_check = memory::aob_scan( file_buffer, file_size, "x????xx?????xxx", { 0xE8, 0x00, 0x00, 0x00, 0x00, 0x80, 0xBF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x74, 0x05, 0xE8 } );
 
 		// nop jz
-		*reinterpret_cast<std::uint16_t*>( file_buffer + scan_soothsayer_check[0] + 7 ) = 0x9090;
+		*reinterpret_cast<std::uint16_t*>( file_buffer + scan_soothsayer_check[0] + 12 ) = 0x9090;
 
 		// set cursor to 0
 		studio.seekg( 0, std::ios::beg );
